fdf972c963fedb8a511b77e8549d748f5950c081
SkylerMayday <skylertjh@gmail.com>
2026-05-19 23:43:48 +0800

    feat: add ParsedCardInfo domain model

---
+++ app/src/main/java/com/skyler/pokedexbinder/domain/ParsedCardInfo.kt
@@ -0,0 +1,9 @@
+package com.skyler.pokedexbinder.domain
+
+data class ParsedCardInfo(
+    val cardName: String?,
+    val cardNumber: String?,
+    val setTotal: String?,
+    val hp: String?,
+    val artist: String?
+)
